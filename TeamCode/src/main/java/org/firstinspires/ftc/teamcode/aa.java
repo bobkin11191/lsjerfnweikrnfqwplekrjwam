9864fa517c42514219ff51b051ab5955a67b171f
9123456787758674569765 <landonroberts707@gmail.com>
2025-10-19 11:56:01 -0700

    Create aa.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/aa.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+public class aa extends OpMode{
+    public CRServo thing;
+    @Override
+    public void init() {
+        thing = hardwareMap.get(CRServo.class, "thingg");
+    }
+
+    @Override
+    public void loop() {
+if (gamepad1.a) {
+    thing.setPower(1);
+}
+if (gamepad1.b) {
+    
+    thing.setPower(0);
+}
+    }
+}
