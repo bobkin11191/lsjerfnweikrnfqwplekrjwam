9931119d26dd580194eac491e36182fd0c6a41a8
9123456787758674569765 <landonroberts707@gmail.com>
2026-01-03 21:06:27 -0800

    Update ARM.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ARM.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ARM.java
@@ -1,4 +1,5 @@
 package org.firstinspires.ftc.teamcode;
+
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -11,14 +12,35 @@ public class ARM extends OpMode{
     public void init() {
        arm = hardwareMap.get(DcMotor.class, "312");
        arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-
-        arm.setTargetPosition(500);
+        arm.setTargetPosition(0);//265
         arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         arm.setPower(.3);
+
     }
 
     @Override
     public void loop() {
+        int MIN_POS = 0;
+        int MAX_POS = 170;
 
+        double trigger = gamepad1.right_trigger;
+        int targetPos = (int)(MIN_POS + trigger * (MAX_POS - MIN_POS));
+        arm.setTargetPosition(targetPos);
+        arm.setPower(0.4);
+        if(gamepad1.a) {
+        arm.setTargetPosition(133);
+            arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            arm.setPower(.3);
+        }
+        if(gamepad1.b) {
+            arm.setTargetPosition(0);
+            arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            arm.setPower(.3);}
+
+
+        telemetry.addData("Trigger", trigger);
+        telemetry.addData("Target", targetPos);
+        telemetry.addData("Current", arm.getCurrentPosition());
+        telemetry.update();
     }
 }
